14a82e76b93ed73afb8a16673d163f2744e97234
Juliano <julianoferrarigianlupi@gmail.com>
2021-02-11 14:15:04 -0500

    added calculations for E and M and options for saving them

--- Ising.py
+++ Ising.py
@@ -11,7 +11,8 @@ class IsingModel:
     """
 
     def __init__(self, dim=100, contact_energy=1, temperature=1, max_sim_time=1000, save_loc=r'./', save_frequency=10,
-                 save_csv=False, save_npz=True, do_plot_state=False, plot_state_frequency=100):
+                 save_csv=False, save_npz=True, do_plot_state=False, plot_state_frequency=100, calc_energy=False,
+                 calc_magn=False, save_energy=False, save_magn=False):
         # dimension
 
         self.N = dim
@@ -24,6 +25,31 @@ class IsingModel:
         self.save_freq = save_frequency
         self.save_csv = save_csv
         self.save_npz = save_npz
+
+        self.calc_energy = calc_energy
+        self.calc_magn = calc_magn
+
+        self.save_energy = save_energy
+        self.save_magn = save_magn
+
+        if save_energy:
+            path = self.save_loc
+            path = os.path.abspath(path)
+            path = os.path.join(path, 'energy_step_' + str(self.mcs)+'.csv')
+            with open(path, 'w') as f:
+                pass
+            e = self.get_state_energy()
+            self.do_save_energy(e)
+
+        if save_magn:
+            path = self.save_loc
+            path = os.path.abspath(path)
+            path = os.path.join(path, 'mag_step_' + str(self.mcs)+'.csv')
+            with open(path, 'w') as f:
+                pass
+            m = self.get_state_magnetization()
+            self.do_save_magnetization(m)
+
 
         if save_csv:
             self.save_state_as_csv()
@@ -52,6 +78,20 @@ class IsingModel:
         path = os.path.abspath(path)
         path = os.path.join(path, file_name + str(self.mcs))
         np.savez_compressed(path + '.npz', self.state)
+
+    def do_save_energy(self, e, file_name='energy_step_'):
+        path = self.save_loc
+        path = os.path.abspath(path)
+        path = os.path.join(path, file_name + str(self.mcs))
+        with open(path, 'a') as f:
+            f.write('{}, {}'.format(self.mcs, e))
+
+    def do_save_magnetization(self, m, file_name='mag_step_'):
+        path = self.save_loc
+        path = os.path.abspath(path)
+        path = os.path.join(path, file_name + str(self.mcs))
+        with open(path, 'a') as f:
+            f.write('{}, {}'.format(self.mcs, m))
 
     def _do_monte_carlo_step(self):
         stt = self.state
@@ -84,17 +124,46 @@ class IsingModel:
     def do_mcs(self):
         self._do_monte_carlo_step()
 
+    def get_state_magnetization(self):
+        return np.sum(self.state)
+
+    def get_state_energy(self):
+        N = self.N
+        # J = self.J
+        stt = self.state
+        e = 0
+        for x in range(N):
+            for y in range(N):
+                spin = stt[x, y]
+                sum_neig_spins = stt[(x + 1) % N, y] + stt[(x - 1) % N, y] + stt[x, (y + 1) % N] + stt[x, (y - 1) % N]
+
+                e += 2 * spin * sum_neig_spins
+        e *= self.J
+
+        return e/4  # quadruple count correction
+
     def run(self):
         for t in range(1, self.mt):
             self.mcs = t
             self.do_mcs()
-            if not self.mcs % self.save_freq and (self.save_csv or self.save_npz):
+
+            time_to_save_the_data = not self.mcs % self.save_freq
+
+            if time_to_save_the_data and (self.save_csv or self.save_npz):
                 if self.save_csv:
                     self.save_state_as_csv()
                 if self.save_npz:
                     self.save_state_as_npz()
+            if time_to_save_the_data and self.save_energy:
+                e = self.get_state_energy()
+                self.do_save_energy(e)
+
+            if time_to_save_the_data and self.save_magn:
+                m = self.get_state_magnetization()
+                self.do_save_magnetization(m)
+
             if self.do_plot_state and not self.mcs % self.plot_state_frequency:
-                plt.close()
+                # plt.close()
                 self.plot_state()
 
 
